840d11bc3ba5b31dea0e6015b71fec48d297ec48
Ash <scorpio@Noisebox2.0>
2019-05-26 14:31:49 -0400

    More window manager functionality

--- Game.py
+++ Game.py
@@ -6,16 +6,17 @@ h = 640
 t = "IDLE CODE"
 scale = 32
 
-def click():
-    print(f"Clicked Canvas: {ids[0]} at X: {win.getMousePos()[0]} Y: {win.getMousePos()[1]}")
+def click(e):
+    print(f"Clicked Canvas: {win.getCan(ids[0])[7]} at X: {win.getMousePos()[0]} Y: {win.getMousePos()[1]}")
 
 ids = [
-    "Click Area",
-    "Upgrade Area"
+    "Click Area", # 0
+    "Upgrade Area", # 1
+    "Console Area" # 2
 ]
 
 keys = [
-    '<Button-1>'
+    '<Button-1>' # Mouse Click
 ]
 
 commands = [
@@ -23,10 +24,11 @@ commands = [
 ]
 
 win = Window.GameWindow(w, h, t)
-win.createCan(m.floor(w/1.25), h, 0, 1, scale, 'grey80', ids[0])
-win.createCan(m.floor(w/2.75), h, 0, 0, scale, 'white', ids[1])
+win.createCan(m.floor(w/1.341), h-160, 0, 5, 6, 5, scale, 'blue', ids[0], 'N' + 'W')
+win.createCan(m.floor(w/4), h, 0, 0, 10, 4, scale, 'white', ids[1], 'W')
+win.createCan(m.floor(w/1.341), 160, 7, 5, 3, 5, scale, 'black', ids[2], 'S' + 'W')
 win.positionCans()
-win.bindKey(keys[0], lambda e=win.getCan(ids[0])[0]: commands[0](), ids[0])
+win.bindKey(keys[0], lambda e : commands[0](e), ids[0])
 
 while True:
     win.Update()
--- Window.py
+++ Window.py
@@ -15,13 +15,34 @@ class GameWindow:
 
         self.cans = {}
 
-    def createCan(self, width, height, row, col, scale, bg, id):
-        self.cans[id] = [tk.Canvas(self.root, width=width, height=height, bg=bg), row, col, scale]
+    def createCan(self, width, height, row, col, rspan, cspan, scale, bg, id, stick=None):
+        self.cans[id] = [
+            tk.Canvas(self.root, width=width, height=height, bg=bg), 
+            row, 
+            col, 
+            rspan, 
+            cspan, 
+            scale, 
+            stick,
+            id
+            ]
     
     def positionCans(self):
         ids = self.cans.keys()
         for id in ids:
-            self.cans[id][0].grid(row=self.cans[id][1], column=self.cans[id][2])
+            if self.cans[id][6] != None:
+                print("Stickied")
+                self.cans[id][0].grid(row=self.cans[id][1],
+                column=self.cans[id][2], 
+                rowspan=self.cans[id][3],
+                columnspan=self.cans[id][4],
+                sticky=(self.cans[id][6]))
+            else:
+                print("Not Stickied")
+                self.cans[id][0].grid(row=self.cans[id][1], 
+                column=self.cans[id][2],
+                rowspan=self.cans[id][3],
+                columnspan=self.cans[id][4])
 
     def disableCan(self, id):
         self.cans[id][0].config(state="disabled")
